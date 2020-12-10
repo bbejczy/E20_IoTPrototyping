5c4f00257f6cce71914e4b501b696bd344f01fb4
BNBJ (Bence Bejczy) <BNBJ@novonordisk.com>
2020-12-10 17:43:27 +0100

    Interface

--- Interface/Interface.ino
+++ Interface/Interface.ino
@@ -1,47 +1,167 @@
 #include "Keypad.h"
-const byte ROWS = 4; //four rows
-const byte COLS = 4; //four columns
+#include "LiquidCrystal_I2C.h"
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
 
+//Initialize keyboard
+const byte ROWS = 2; //four rows
+const byte COLS = 4; //four columns
 char keys[ROWS][COLS] = {
   {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+  {'4','5','6','B'}
 };
 
-//// For Arduino Microcontroller
-//byte rowPins[ROWS] = {9, 8, 7, 6}; 
-//byte colPins[COLS] = {5, 4, 3, 2}; 
+// Initialize screen
+byte rowPins[ROWS] = {9, 8}; 
+byte colPins[COLS] = {7, 6, 5, 4}; 
 
-// For ESP8266 Microcontroller
-//GPIO 1 (TX) swap the pin to a GPIO.
-pinMode(1, FUNCTION_3);
-//GPIO 3 (RX) swap the pin to a GPIO.
-pinMode(3, FUNCTION_3);
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+LiquidCrystal_I2C lcd(0x3F, 16, 2); 
 
-  byte rowPins[ROWS] = {3, 1, D3, D4}; 
-  byte colPins[COLS] = {D5, D6, D7, D8};
+// Declaration of variables
+char keyInput;
+const int len_password = 4;
+char master_password[len_password] = {'1','2','3','4'};
+char attempt_password[len_password];
+int cursor_pass = 0;
+bool unlocked = false;
+bool initialized = false;
+bool exitLoop = false;
+int screenID = 0;
+String screens [3][2] = {{"Choose an option","Hum 1, Temp 2"}, {"Humidity", "X %"}, {"Temperature","X Celsius"}};
+int i = 0;
 
-  Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-//
-//byte rowPins[ROWS] = {3, 1, D3, D4}; 
-//byte colPins[COLS] = {D5, D6, D7, D8}; 
+// Declaration of functions
+void menu(){
+  lcd.setCursor(0,0);
+  lcd.print(screens[screenID][0]);
+  lcd.setCursor(0,1);
+  lcd.print(screens[screenID][1]);
+  
+  keyInput = keypad.getKey();
+  //keyPress(keyInput);
 
-// For ESP32 Microcontroller
-//byte rowPins[ROWS] = {23, 22, 3, 21}; 
-//byte colPins[COLS] = {19, 18, 5, 17};
+    switch (keyInput) {
+      case '1':
+        lcd.clear();
+        screenID = 1;
+        break;
+      case '2':
+        lcd.clear();
+        screenID = 2;
+        break;
+      case 'B':
+        lcd.clear();
+        screenID = 0;
+      default:
+        break;
+    }
+}
 
+void keyPress(char key){
+  
+  if (key){
+        //Serial.println(key);
+        //lcd.clear();
+        lcd.setCursor(0,1);
+        lcd.print(key);
+        delay(100);
+    
+  }
+}
 
-
-void setup() {
-  Serial.begin(9600);
+void checkPASS()
+{
+   int correct=0;
+   int i;
+   for (i=0; i<len_password; i++) {
+    if (attempt_password[i]==master_password[i]) {
+      correct++;
+      }
+    }
+   if (correct==len_password && cursor_pass==len_password){
+    lcd.setCursor(0,1);
+    lcd.print("Correct Key");
+    delay(3000);
+    unlocked = true;
+    cursor_pass=0;
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Insert Password");
+   }
+   else
+   {
+    lcd.setCursor(0,1);
+    lcd.print("Incorrect Key");
+    delay(3000);
+    cursor_pass=0;
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Insert Password");
+   }
+   for (int zz=0; zz<len_password; zz++) {
+    attempt_password[zz]=0;
+   }
 
 }
 
-void loop() {
-  char key = keypad.getKey();
+// Main script
 
-  if (key){
-    Serial.println(key);
+void setup() {
+   Serial.begin(115200);
+   lcd.begin(16,2);
+   lcd.init();
+   lcd.backlight();
+   lcd.setCursor(0,0);
+   lcd.print("Initializing setup");
+   delay(500);
+   lcd.clear();
+   lcd.setCursor(0,0);
+   lcd.print("Enter password:");
+}
+
+void loop() {
+  
+
+  while(unlocked == false){
+    keyInput = keypad.getKey();
+    lcd.setCursor(cursor_pass-1,1);
+    lcd.print("*");
+    if (keyInput){
+      switch(keyInput){
+        case 'A':
+          cursor_pass=0;
+          lcd.setCursor(0,1);
+          lcd.print("                ");
+          break;
+        case 'B':
+          delay(100); // added debounce
+          checkPASS();
+          break;
+        default:
+          attempt_password[cursor_pass]=keyInput;
+          cursor_pass++;
+        }
+    }
+
+  };
+  
+  //lcd.clear();
+  
+  if(initialized ==false){ // entry message
+    lcd.setCursor(0,0);
+    lcd.print("Device unlocked");
+    delay(1000);
+    initialized = true;
+    lcd.clear();
   }
+
+  menu();
+
+
+  // keyInput = keypad.getKey();
+  // keyPress(keyInput);
+
+
 }
